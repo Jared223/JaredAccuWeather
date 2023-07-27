936cf7fdeb653077798f77192f43dcaff523fd9d
Jared223 <jaredpollack@hotmail.co.uk>
2023-07-27 22:55:34 +0100

    Working app.py takes data from OpenWeather API and stores it in a database

---
+++ code/app.py
@@ -0,0 +1,62 @@
+import requests
+import sqlite3
+
+# Setting up and connecting to database
+
+conn = sqlite3.connect('weather_data.db')
+
+cur = conn.cursor()
+
+# Create table
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS Weather (
+        id INTEGER PRIMARY KEY,
+        location TEXT,
+        weather_main TEXT,
+        weather_description TEXT,
+        temperature_c REAL
+    )
+''')
+
+# Setting latitude and longitude information, and API Key
+
+lat = 51.51
+lon = -0.12
+apiKey = 'e1985d57baf12fc90046cb533d0e2a20'
+
+# Set API url
+
+url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}'
+
+# Request information to openweathermap
+
+r = requests.get(url)
+
+# Get response
+
+response = r.json()
+
+# Setting response variables
+
+weather_main = response['weather'][0]['main']
+weather_description = response['weather'][0]['description']
+
+temperature = response['main']['temp']
+
+temperature_celsius = temperature - 273.15
+temperature_celsius = round(temperature_celsius, 1)
+
+location = response['name']
+
+# Insert weather data into table
+cur.execute('''
+    INSERT INTO Weather (location, weather_main, weather_description, temperature_c) 
+    VALUES (?, ?, ?, ?)
+''', (location, weather_main, weather_description, temperature_celsius))
+
+# Commit changes
+conn.commit()
+
+# Close connection
+conn.close()
+
